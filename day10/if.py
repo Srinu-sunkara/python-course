ea88e6cec18c9f3b6f8214a529045e8f1eb83191
Srinivas sunkara <srinusunkara9951@gmail.com>
2026-08-17 17:54:01 +0530

    conditional statements

---
+++ day10/if.py
@@ -0,0 +1,21 @@
+'''sales = int(input('enter the sales:'))
+if sales>1000:
+
+    print('best seller')'''
+
+''''eli_acc = eval(input('eligible account:'))
+var_sub = eval(input('verified subscription:'))
+if eli_acc and var_sub:
+    print('verified badge granted') '''
+'''rain_status = eval(input('enter the rain status'))
+if rain_status:
+    print('extra charges applied')'''
+username = input('username: ')
+password = input('password: ')
+if username == 'admin' and 'password' == 'admin1':
+    print('login successful')
+else:
+    print('tbrgtjbvfgf')
+
+    
+
